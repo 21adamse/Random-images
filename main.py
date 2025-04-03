facf0c3b4a02019a486011bf610ff8ac03038b65
21adamse <EwanAd2010@gmail.com>
2025-04-03 16:08:56 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,26 +2,28 @@ import pgzrun
 import random
 WIDTH=700
 HEIGHT=400
+
 x1=random.randint(100,600)
-y1=random.randint(100,400)
+y1=random.randint(100,350)
 x2=random.randint(100,600)
-y2=random.randint(100,400)
+y2=random.randint(100,350)
 x3=random.randint(100,600)
-y3=random.randint(100,400)
+y3=random.randint(100,350)
 x4=random.randint(100,600)
-y4=random.randint(100,400)
+y4=random.randint(100,350)
 x5=random.randint(100,600)
-y5=random.randint(100,400)
+y5=random.randint(100,350)
 x6=random.randint(100,600)
-y6=random.randint(100,400)
+y6=random.randint(100,350)
 x7=random.randint(100,600)
-y7=random.randint(100,400)
+y7=random.randint(100,350)
 x8=random.randint(100,600)
-y8=random.randint(100,400)
+y8=random.randint(100,350)
 x9=random.randint(100,600)
-y9=random.randint(100,400)
+y9=random.randint(100,350)
 x10=random.randint(100,600)
-y10=random.randint(100,400)
+y10=random.randint(100,350)
+
 shape1 = Actor("circle")
 shape1.pos = ( x1,y1 )
 shape2 = Actor("cylinder")
@@ -42,6 +44,23 @@ shape9 = Actor("trapezium")
 shape9.pos = ( x9,y9)
 shape10 = Actor("triangle")
 shape10.pos = (x10,y10)
+
+numshapes=10
+nextshape=0
+shapes=[shape1,shape2,shape3,shape4,shape5,shape6,shape7,shape8,shape9,shape10]
+lines=[]
+
+def on_mouse_down(pos):
+    global nextshape, lines
+    if nextshape < numshapes:
+        if shapes[nextshape].collidepoint(pos):
+            if nextshape:
+                lines.append((shapes[nextshape-1].pos,shapes[nextshape].pos))
+            nextshape += 1
+        else:
+            nextshape = 0
+            lines = []
+
 def draw():
     screen.clear()
     shape1.draw()
@@ -64,4 +83,6 @@ def draw():
     screen.draw.text("9",(x9,y9+30))
     shape10.draw()
     screen.draw.text("10",(x10,y10+30))
+    for line in lines:
+        screen.draw.line(line[0],line[1],"white")
 pgzrun.go()
